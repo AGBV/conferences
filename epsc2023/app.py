02c038d4f8867778e793a8b45e81d737d64c5aea
Mirza <21687187+arunoruto@users.noreply.github.com>
2023-07-24 20:15:43 +0200

    Increase size a bit

--- epsc2023/app.py
+++ epsc2023/app.py
@@ -146,5 +146,5 @@ for i in range(rows):
 
 plt.tight_layout()
 fig_html = mpld3.fig_to_html(fig)
-components.html(fig_html, height=1000, scrolling=True)
+components.html(fig_html, height=1050, scrolling=True)
 st.info('**Note:** hover to the bottom left corner for zoom and pan tools.')
